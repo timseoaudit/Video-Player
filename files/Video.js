9e5aef64165c8ba7162a48f622df7f0207cd4cb9
Timneji <139955017+timseoaudit@users.noreply.github.com>
2023-09-18 18:05:23 +0100

    init resp

---
+++ files/Video.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Video( {src} ) {
+    return (
+      <div>
+        <video controls autostart autoPlay muted src={src} />
+      </div>
+    );
+};
+export default Video;
